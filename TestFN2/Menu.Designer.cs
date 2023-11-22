e9fd001ea01bdfb2264386ba2247a361af20a245
MatheusMhc <Matheus.daSilva@mymhc.ca>
2023-11-22 13:15:05 -0700

    creating menu  second

--- TestFN2/Menu.Designer.cs
+++ TestFN2/Menu.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            lblExit = new Label();
+            lblAbout = new Label();
+            lblNewGame = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Snap ITC", 46F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(317, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(454, 80);
+            label1.TabIndex = 1;
+            label1.Text = "Snake Game";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.snake1_inPixio__1_;
+            pictureBox1.Location = new Point(12, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(437, 436);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // lblExit
+            // 
+            lblExit.AutoSize = true;
+            lblExit.Font = new Font("Snap ITC", 26F, FontStyle.Regular, GraphicsUnit.Point);
+            lblExit.Location = new Point(506, 296);
+            lblExit.Name = "lblExit";
+            lblExit.Size = new Size(112, 45);
+            lblExit.TabIndex = 4;
+            lblExit.Text = "Exit";
+            lblExit.MouseLeave += lblExit_MouseLeave;
+            lblExit.MouseMove += lblExit_MouseMove;
+            // 
+            // lblAbout
+            // 
+            lblAbout.AutoSize = true;
+            lblAbout.Font = new Font("Snap ITC", 26F, FontStyle.Regular, GraphicsUnit.Point);
+            lblAbout.Location = new Point(491, 237);
+            lblAbout.Name = "lblAbout";
+            lblAbout.Size = new Size(137, 45);
+            lblAbout.TabIndex = 5;
+            lblAbout.Text = "About";
+            lblAbout.MouseLeave += lblAbout_MouseLeave;
+            lblAbout.MouseMove += lblAbout_MouseMove;
+            // 
+            // lblNewGame
+            // 
+            lblNewGame.AutoSize = true;
+            lblNewGame.Font = new Font("Snap ITC", 26F, FontStyle.Regular, GraphicsUnit.Point);
+            lblNewGame.Location = new Point(455, 182);
+            lblNewGame.Name = "lblNewGame";
+            lblNewGame.Size = new Size(228, 45);
+            lblNewGame.TabIndex = 6;
+            lblNewGame.Text = "New Game";
+            lblNewGame.MouseClick += lblNewGame_MouseClick;
+            lblNewGame.MouseLeave += lblNewGame_MouseLeave;
+            lblNewGame.MouseMove += lblNewGame_MouseMove;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblNewGame);
+            Controls.Add(lblAbout);
+            Controls.Add(lblExit);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Menu";
             Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label lblExit;
+        private Label lblAbout;
+        private Label lblNewGame;
     }
 }
--- TestFN2/Menu.cs
+++ TestFN2/Menu.cs
@@ -16,5 +16,44 @@ namespace TestFN2
         {
             InitializeComponent();
         }
+
+        private void lblNewGame_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblNewGame.ForeColor = Color.Brown;
+        }
+
+        private void lblNewGame_MouseLeave(object sender, EventArgs e)
+        {
+            lblNewGame.ForeColor = Color.Black;
+        }
+
+        private void lblExit_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblExit.ForeColor = Color.Brown;
+        }
+
+        private void lblExit_MouseLeave(object sender, EventArgs e)
+        {
+            lblExit.ForeColor = Color.Black;
+        }
+
+        private void lblAbout_MouseLeave(object sender, EventArgs e)
+        {
+            lblAbout.ForeColor = Color.Black;
+        }
+
+        private void lblAbout_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblAbout.ForeColor = Color.Brown;
+        }
+
+        private void lblNewGame_MouseClick(object sender, MouseEventArgs e)
+        {
+            new Form1().Show();
+            this.Hide();
+
+        }
+
+
     }
 }
