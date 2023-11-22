c4fdc6e5ecf1dcd5cdb3ed49bf4f05caab43edec
MatheusMhc <Matheus.daSilva@mymhc.ca>
2023-11-22 12:31:39 -0700

    menu creating

--- TestFN2/Properties/Resources.Designer.cs
+++ TestFN2/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace TestFN2.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap snake1_inPixio__1_ {
+            get {
+                object obj = ResourceManager.GetObject("snake1_inPixio (1)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
