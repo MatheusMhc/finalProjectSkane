28de28dbb3c254dd0428b6a1ac75ba8ffb63b2e6
MatheusMhc <Matheus.daSilva@mymhc.ca>
2023-11-29 16:02:18 -0700

    change the name game project

--- TestFN2/Properties/Resources.Designer.cs
+++ TestFN2/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SnakeGameSpace.Properties {
+namespace SnakeGameProjec.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SnakeGameSpace.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TestFN2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SnakeGameProjec.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
